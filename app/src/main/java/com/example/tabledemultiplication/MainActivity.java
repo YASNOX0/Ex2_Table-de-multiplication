44135b33dd82595eebe2f2eca647ec0233a3a75b
YASNOX0 <121507365+YASNOX0@users.noreply.github.com>
2023-01-09 19:25:18 +0100

    modify : MainActivity.java(btn_reinitialiser & btn_afficher programmed)

--- app/src/main/java/com/example/tabledemultiplication/MainActivity.java
+++ app/src/main/java/com/example/tabledemultiplication/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.tabledemultiplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText number ;
+    Button btn_reinitialiser , btn_quitter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        number = findViewById(R.id.number);
+        btn_reinitialiser = findViewById(R.id.btn_reinitialiliser);
+        btn_quitter = findViewById(R.id.btn_quitter);
+
+        btn_reinitialiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                number.setText("");
+                number.getText().clear();
+            }
+        });
+
+        btn_quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
