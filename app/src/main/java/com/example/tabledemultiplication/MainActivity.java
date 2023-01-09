b092eb3768964834fdd79085f55b4a88a21b01b8
YASNOX0 <121507365+YASNOX0@users.noreply.github.com>
2023-01-09 23:22:50 +0100

    fix bugs : MainActivity.java (btn_ reinitialiser programmed) && activity_main.xml(textView id added)

--- app/src/main/java/com/example/tabledemultiplication/MainActivity.java
+++ app/src/main/java/com/example/tabledemultiplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.tabledemultiplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
         btn_reinitialiser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                number.setText("");
-                number.getText().clear();
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
+
             }
         });
 
