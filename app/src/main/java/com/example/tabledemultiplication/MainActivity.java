505fab77f15a6bd80f0f6d5466f2951b4d07113a
YASNOX0 <121507365+YASNOX0@users.noreply.github.com>
2023-01-09 23:34:55 +0100

    modify : MainActivity.java(change background buttons programmed) && activity_main.xml(constraint layout id added)

--- app/src/main/java/com/example/tabledemultiplication/MainActivity.java
+++ app/src/main/java/com/example/tabledemultiplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tabledemultiplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,15 +11,23 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText number ;
-    Button btn_reinitialiser , btn_quitter;
+    EditText number;
+    Button btn_reinitialiser, btn_quitter, btn_white, btn_pink, btn_lavender;
+    ConstraintLayout constraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         number = findViewById(R.id.number);
         btn_reinitialiser = findViewById(R.id.btn_reinitialiliser);
         btn_quitter = findViewById(R.id.btn_quitter);
+        btn_white = findViewById(R.id.btn_white);
+        btn_pink = findViewById(R.id.btn_pink);
+        btn_lavender = findViewById(R.id.btn_lavender);
+        constraintLayout = findViewById(R.id.constraintLayout);
+
 
         btn_reinitialiser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +43,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btn_white.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                constraintLayout.setBackgroundResource(R.color.white);
+            }
+        });
+
+        btn_pink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                constraintLayout.setBackgroundResource(R.color.Pink);
+            }
+        });
+
+        btn_lavender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                constraintLayout.setBackgroundResource(R.color.Lavender);
             }
         });
     }
